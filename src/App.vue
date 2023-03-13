8f91be1b64803a6b3c850a199a151aac13ec9faf
frankhood <frankhood@iMacLate2015.lan>
2023-03-13 10:32:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'You need to know how to google stuff!',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
